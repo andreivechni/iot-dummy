17fcbea9fa168b498f8b2784da774aa53f0fdb4f
Andrei Aleksandrov <krasnijlajm@gmail.com>
2019-12-17 21:07:08 +0300

    WIP rest API

--- app.js
+++ app.js
@@ -1,19 +1,73 @@
 const Koa = require('koa');
 const route = require('koa-route');
-var bodyParser = require('koa-bodyparser');
+const bodyParser = require('koa-bodyparser');
+
 const app = new Koa();
+
 app.use(bodyParser());
 
-const main = ctx => {
+// DB biometrcis PUSH
+const pushBiometrics = biometricObj => {
+
+    console.log('pushing biometrics to DB:');
+    console.log(JSON.stringify(biometricObj));
+
+}
+
+// DB most recent timeStamp PULL
+const pullMostRecentTimeStamp = (mockErr = false) => {
+
+    if (mockErr) throw new Error('Fake error for tests!!!')
+
+    const lastTimeStampDummy = {
+
+        Timestamp: 1576490100,
+        DeviceId: 1,
+        UserId: 1,
+        Intensity: 0.011111111380159855,
+        Steps: 0,
+        Kind: 0,
+        HeartRate: 0
+
+    }
     
-    console.log(ctx.request.body);
-    ctx.response.type = 'json';
-    ctx.response.body = { code: 200, recieved: ctx.request.body};
+    return lastTimeStampDummy.Timestamp;
 
 };
 
-app.use(route.get('/', main));
-app.use(route.post('/', main));
+// post recieved biometrics
+const postBiometrics = ctx => {
+
+    const recieved = ctx.request.body;
+
+    try {
+
+        pushBiometrics(recieved);
+
+    } catch(err) {
+        
+        ctx.response.body = { code: 500, recieved };
+
+    }
+    
+    ctx.response.body = { code: 200, recieved };
+
+};
+
+// get most recent timeStamp
+const getMostRecentTimeStamp = ctx => {
+
+    const mostRecentTimeStamp = pullMostRecentTimeStamp();
+
+    ctx.response.type = 'json';
+    ctx.response.body = { mostRecentTimeStamp };
+
+}
+
+
+// routes
+app.use(route.post('/biometrics', postBiometrics));
+app.use(route.get('/biometrics/mostrecent', getMostRecentTimeStamp));
 
 app.listen(3000);
 
